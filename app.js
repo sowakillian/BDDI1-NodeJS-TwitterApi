81bd737c7c90fefd8f7a7dd13bf97faa1e41423d
Sowa Killian <killian.sowa@m6.fr>
2020-01-23 10:04:49 +0100

    twitterStream edited

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const { server } = require("./lib/server");
 const twitterStream = require("./lib/twitter");
 
+twitterStream.on("error", error => {
+    console.error(error);
+});
+
 server.listen(8080);
